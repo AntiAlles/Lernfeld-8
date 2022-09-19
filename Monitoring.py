ffe00cdcf07113373e04386999211f212530e7da
K <kaybart@hotmail.de>
2022-09-19 12:26:05 +0200

    Remove TOKEN BLYAT

--- Monitoring.py
+++ Monitoring.py
@@ -1,7 +1,7 @@
 from multiprocessing.dummy import current_process
 from threading import current_thread
 from linode_api4 import LinodeClient
-toekn = "014415c4e2a9f7049f4b7558f13b8cc6e6f5bc686ad88eb0c3fc10b1cff787f8"
+toekn = ""
 client = LinodeClient(toekn)
 
 Linode_ID = 35495125
