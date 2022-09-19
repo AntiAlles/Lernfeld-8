f39735b1e9c7ca953eb9d32791bd3e66aef95b7b
K <kaybart@hotmail.de>
2022-09-19 12:22:23 +0200

    Remove API Keys

--- Monitoring.py
+++ Monitoring.py
@@ -4,9 +4,9 @@ from linode_api4 import LinodeClient
 toekn = "014415c4e2a9f7049f4b7558f13b8cc6e6f5bc686ad88eb0c3fc10b1cff787f8"
 client = LinodeClient(toekn)
 
-Linode_ID = client.networking.ips()
-    ##my_linodes = client.load(Linode_ID)
+Linode_ID = 35495125
+Current_load = client.account.invoices()
 
-for i in Linode_ID:
-    ##network = client.load(i)
+for i in Current_load:
+##    network = client.get(any)
     print(i)
