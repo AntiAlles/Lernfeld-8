b65332617943d3ee570a0c07330b15655e1b31d9
K <kaybart@hotmail.de>
2022-09-19 12:44:10 +0200

    Add current load var

--- Monitoring.py
+++ Monitoring.py
@@ -1,12 +1,12 @@
 from multiprocessing.dummy import current_process
 from threading import current_thread
-from linode_api4 import LinodeClient
-toekn = ""
+from linode_api4 import *
+toekn = "014415c4e2a9f7049f4b7558f13b8cc6e6f5bc686ad88eb0c3fc10b1cff787f8"
 client = LinodeClient(toekn)
 
 Linode_ID = 35495125
-Current_load = client.account.invoices()
+my_instance = Instance(client, Linode_ID)
 
-for i in Current_load:
-##    network = client.get(any)
-    print(i)
+##for i in Current_load:
+Current_load = my_instance.stats
+print(Current_load)
