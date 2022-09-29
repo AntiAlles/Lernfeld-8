8af7af47495f1ca089b51fbcdf729fb104c8d582
Anti <kaybart@hotmail.de>
2022-09-29 16:14:24 +0200

    feat: add new main and secondary functions and multi threading

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
-from monitor import Monitor
+from monitor import Monitor, Display
 from alarm import Alarm
 from dotenv import load_dotenv
+from threading import Thread
 import time
 
 load_dotenv()
 
-while True:    
-    # function to get the IDs of the Linode Instances 
-    data = Monitor.get_instance_stats()
 
-    # function to update the log file and print current stats to the prompt
-    Monitor.update_log(data)
+def main():
+    while True:    
+        # function to get the IDs of the Linode Instances 
+        data = Monitor.get_instance_stats()
 
-    # function to scan for CPU Load over the max_CPU_load Value given
-    Alarm.is_CPU_over_threshold(data, 80)
+        # function to update the log file and print current stats to the prompt
+        Monitor.update_log(data)
 
-    # function to scan for IO load over the max_IO_usage value given
-    Alarm.is_IO_over_threshold(data, 4)
-    print("Run Done")
-    time.sleep(300)
+        # function to scan for CPU Load over the max_CPU_load Value given
+        Alarm.is_CPU_over_threshold(data, 80)
+
+        # function to scan for IO load over the max_IO_usage value given
+        Alarm.is_IO_over_threshold(data, 4)
+
+        # Wait for 10 seconds
+        time.sleep(10)
+
+def secondary():
+        print("Please enter what you want to Display:")
+
+        while True:
+            #get stats dir from instance 
+            data = Monitor.get_instance_stats()
+            #wait for input in the prompt and convert all lowercase Chars
+            prompt_input = str.lower(input())
+
+            if prompt_input == "-h" or prompt_input == "":
+                print("You can display performance stats by typing one of the 4 into the prompt:\n [cpu]    [io]    [ipv4]    [ipv6]")  
+
+            elif prompt_input == "cpu":
+                Display.display_cpu(data)
+
+            elif prompt_input == "io":
+                Display.display_io(data)
+
+            elif prompt_input == "ipv4":
+                Display.display_ipv4(data)
+
+            elif prompt_input == "ipv6":
+                Display.display_ipv6(data)
+
+#declares variable names for the new threads
+main_thread = Thread(target=main)
+secondary_thread = Thread(target=secondary)
+
+#starts the previously declared threads
+main_thread.start()
+secondary_thread.start()
--- monitor.py
+++ monitor.py
@@ -38,35 +38,11 @@ class Monitor():
         # Convert first array item of Cpu into integer and divide it by 1000 Print UTC timestamp of server With Year-Month-Day Hour-Minute-Seconds
         timestamp = timedate.utcfromtimestamp(int(cpu[0]/1000)).strftime('%Y-%m-%d %H:%M:%S')
 
-        message = str(timestamp) + "; CPU:" + str(cpu[1]) + "%; " + "io:" + str(io[1]) + "B/s; " + "IPv4: " + str(ipv4[1]) + "Bits/s; " + "IPv6: " + str(ipv6[1]) + "Bits/s"
+        message = str(timestamp) + "; CPU:" + str(cpu[1]) + "%; " + "io:" + str(io[1]) + "Blocks/s; " + "IPv4: " + str(ipv4[1]) + "Bits/s; " + "IPv6: " + str(ipv6[1]) + "Bits/s"
         logging.critical(message)
 
 class Display():
 
-    def display_main():
-        print("Please enter what you want to Display:")
-
-        while True:
-            #get stats dir from instance 
-            data = Monitor.get_instance_stats()
-            #wait for input in the prompt and convert all lowercase Chars
-            prompt_input = str.lower(input())
-
-            if prompt_input == "-h":
-                print("You can display performance stats by typing one of the 4 into the prompt:\n [cpu]    [io]    [ipv4]    [ipv6]")  
-
-            elif prompt_input == "cpu":
-                Display.display_cpu(data)
-
-            elif prompt_input == "io":
-                Display.display_io(data)
-
-            elif prompt_input == "ipv4":
-                Display.display_ipv4(data)
-
-            elif prompt_input == "ipv6":
-                Display.display_ipv6(data)
-                
     def display_cpu(data):
 
         #print the variable in last place of cpu in the data dir
@@ -81,7 +57,7 @@ class Display():
         io = data["data"]["io"]["io"][-1]
 
         # Convert first array item of Cpu into integer and divide it by 1000 Print UTC timestamp of server With Year-Month-Day Hour-Minute-Seconds
-        print(str(timedate.utcfromtimestamp(int(io[0]/1000)).strftime('%Y-%m-%d %H:%M:%S')) + " CPU Load is " + str(io[1]) + "%")
+        print(str(timedate.utcfromtimestamp(int(io[0]/1000)).strftime('%Y-%m-%d %H:%M:%S')) + " io Load is " + str(io[1]) + "Blocks/s")
     
     def display_ipv4(data):
 
@@ -89,7 +65,7 @@ class Display():
         ipv4 = data["data"]["netv4"]["in"][-1]
 
         # Convert first array item of Cpu into integer and divide it by 1000 Print UTC timestamp of server With Year-Month-Day Hour-Minute-Seconds
-        print(str(timedate.utcfromtimestamp(int(ipv4[0]/1000)).strftime('%Y-%m-%d %H:%M:%S')) + " CPU Load is " + str(ipv4[1]) + "%")
+        print(str(timedate.utcfromtimestamp(int(ipv4[0]/1000)).strftime('%Y-%m-%d %H:%M:%S')) + " IPv4 Load is " + str(ipv4[1]) + "Bits/s")
    
     def display_ipv6(data):
 
@@ -97,4 +73,4 @@ class Display():
         ipv6 = data["data"]["netv6"]["in"][-1]
 
         # Convert first array item of Cpu into integer and divide it by 1000 Print UTC timestamp of server With Year-Month-Day Hour-Minute-Seconds
-        print(str(timedate.utcfromtimestamp(int(ipv6[0]/1000)).strftime('%Y-%m-%d %H:%M:%S')) + " CPU Load is " + str(ipv6[1]) + "%")
+        print(str(timedate.utcfromtimestamp(int(ipv6[0]/1000)).strftime('%Y-%m-%d %H:%M:%S')) + " IPv6 Load is " + str(ipv6[1]) + "Bits/s")
